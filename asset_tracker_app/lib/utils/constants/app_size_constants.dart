6211b9e2b9d88235e527958b441cabab3aacf05c
ErenBal <erenbal.academic@gmail.com>
2024-12-10 16:00:25 +0300

    App size constants added.

--- asset_tracker_app/lib/utils/constants/app_size_constants.dart
+++ asset_tracker_app/lib/utils/constants/app_size_constants.dart
@@ -1,19 +1,21 @@
 class AppSize {
-  // Icon boyutları
+  // Icon
   static const double iconSmall = 16.0;
   static const double iconMedium = 24.0;
   static const double iconLarge = 32.0;
   static const double iconXL = 48.0;
   static const double iconXXL = 80.0;
+  static const double iconXXXL = 100.0;
 
-  // Button yükseklikleri
+  // Button
   static const double loginButtonHeight = 48.0;
   static const double buttonRadius = 8.0;
 
-  // Text boyutları
+  // Text
   static const double textSmall = 12.0;
   static const double textMedium = 14.0;
   static const double textLarge = 16.0;
   static const double textXL = 20.0;
   static const double textXXL = 28.0;
+  static const double textXXXL = 32;
 }
