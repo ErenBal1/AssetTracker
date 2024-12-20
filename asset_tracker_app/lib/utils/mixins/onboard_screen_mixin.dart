d125a4af65d9c283e8dc3fc0bd37242bd64f26ff
ErenBal <erenbal.academic@gmail.com>
2024-12-20 18:53:13 +0300

    State management (Bloc) added. main.dart refactored.

--- asset_tracker_app/lib/utils/mixins/onboard_screen_mixin.dart
+++ asset_tracker_app/lib/utils/mixins/onboard_screen_mixin.dart
@@ -1,12 +1,8 @@
-import 'package:asset_tracker_app/localization/strings.dart';
-import 'package:asset_tracker_app/utils/constants/app_routes_constants.dart';
-import 'package:asset_tracker_app/utils/constants/controllers.dart';
-import 'package:asset_tracker_app/utils/constants/onboarding_page_list.dart';
 import 'package:flutter/material.dart';
+import '../constants/controllers.dart';
 
 mixin OnboardScreenMixin<T extends StatefulWidget> on State<T> {
   int currentPage = 0;
-  final onboardingData = OnboardingData();
 
   @override
   void dispose() {
@@ -20,26 +16,10 @@ mixin OnboardScreenMixin<T extends StatefulWidget> on State<T> {
     });
   }
 
-  void onNextPressed() {
-    if (currentPage < onboardingData.pages.length - 1) {
-      navigateToNextPage();
-    } else {
-      navigateToLogin();
-    }
-  }
-
   void navigateToNextPage() {
     pageController.nextPage(
       duration: const Duration(milliseconds: 300),
       curve: Curves.easeInOut,
     );
   }
-
-  void navigateToLogin() {
-    Navigator.pushReplacementNamed(context, ToScreen.loginPage);
-  }
-
-  String get buttonText => currentPage == onboardingData.pages.length - 1
-      ? LocalStrings.getStartedButton
-      : LocalStrings.nextButton;
 }
--- asset_tracker_app/lib/view/splash_view.dart
+++ asset_tracker_app/lib/view/splash_view.dart
@@ -1,10 +1,14 @@
+import 'package:asset_tracker_app/bloc/splash/splash_bloc.dart';
+import 'package:asset_tracker_app/bloc/splash/splash_state.dart';
 import 'package:asset_tracker_app/localization/strings.dart';
+import 'package:asset_tracker_app/utils/constants/app_routes_constants.dart';
+import 'package:asset_tracker_app/utils/constants/theme/constant_gap_sizes.dart';
 import 'package:asset_tracker_app/utils/constants/theme/constant_sizes.dart';
 import 'package:asset_tracker_app/utils/constants/theme/constant_text_styles.dart';
-import 'package:asset_tracker_app/utils/constants/theme/constant_gap_sizes.dart';
 import 'package:asset_tracker_app/utils/mixins/splash_screen_mixin.dart';
 import 'package:asset_tracker_app/widgets/custom_icon_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SplashScreenView extends StatefulWidget {
   const SplashScreenView({super.key});
@@ -17,18 +21,27 @@ class _SplashScreenViewState extends State<SplashScreenView>
     with SplashScreenMixin {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            CustomIcon(icon: Icons.inventory, size: ConstantSizes.iconXXXL),
-            GapSize.medium(),
-            Text(
-              LocalStrings.appLabel,
-              style: ConstantTextStyles.headlineLarge,
-            )
-          ],
+    return BlocListener<SplashBloc, SplashState>(
+      listener: (context, state) {
+        if (state is NavigateToOnboarding) {
+          Navigator.pushReplacementNamed(context, ToScreen.onboardPage);
+        } else if (state is NavigateToLogin) {
+          Navigator.pushReplacementNamed(context, ToScreen.loginPage);
+        }
+      },
+      child: const Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomIcon(icon: Icons.inventory, size: ConstantSizes.iconXXXL),
+              GapSize.medium(),
+              Text(
+                LocalStrings.appLabel,
+                style: ConstantTextStyles.headlineLarge,
+              )
+            ],
+          ),
         ),
       ),
     );
