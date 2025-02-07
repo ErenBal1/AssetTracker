7fd6bb0c4c9bd4091b5025376e6a98c2c4cc8e1c
ErenBal <erenbal.academic@gmail.com>
2025-02-07 21:38:34 +0300

    firebase auth added again.

--- asset_tracker_app/lib/init/app_init.dart
+++ asset_tracker_app/lib/init/app_init.dart
@@ -1,7 +1,12 @@
+import 'package:asset_tracker_app/services/firebase/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 final class AppInit {
   static Future<void> initialize() async {
     WidgetsFlutterBinding.ensureInitialized();
+    // await Firebase.initializeApp(
+    //   options: DefaultFirebaseOptions.currentPlatform,
+    // );
   }
 }
